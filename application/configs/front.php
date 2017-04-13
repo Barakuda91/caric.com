06cde91c63d68f2c3e3aaffb547beb70ef7c84d6
Barakuda91 <barakudatm@gmail.com>
2017-04-13 23:52:18 +0300

    Forge commit. 3

---
+++ application/configs/front.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    'siteUtl' => 'http://caric.com',
+    'serverPort' => 8080,
+    'socketPort' => 7788,
+    // настройки вида главной страницы
+    'index' => [
+        'top' => [
+            'tires' => [],
+            'wheels' => [],
+            'auto'  => [
+                'VOLVO',
+                'NISSAN',
+                'TOYOTA',
+                'VOLKSWAGEN',
+                'OPEL',
+                'FORD',
+                'RENAULT',
+                'PEUGEOT',
+                'AUDI',
+                'MERCEDES_BENZ',
+                'FIAT',
+                'BMW',
+                'CHEVROLET',
+                'MITSUBISHI',
+            ],
+            'brands' => [],
+            'spaces' => []
+        ]
+    ],
+    // настройки пагинации
+    'pagination' => [
+        // количество элементов на странице полных списков
+        'manufactureList' => 25
+    ]
+];
